2a44d081a0739f4d38f240ad07eb2ca173417fd5
dien-nguyen <truongdien99@gmail.com>
2019-07-07 12:57:17 +0700

    Update Global.asax.cs

--- GamePortal.API/Global.asax.cs
+++ GamePortal.API/Global.asax.cs
@@ -21,9 +21,9 @@ namespace GamePortal.API
             GlobalConfiguration.Configure(WebApiConfig.Register);
             PayIndexCounter.Init();
             NLogManager.LogMessage("***** START SERVER *****");
-            DBHelper db = new DBHelper(GateConfig.DbConfig);
-            NLogManager.LogMessage("Connect db: " + db.OpenConnection().State);
-            db.Close();
+            //DBHelper db = new DBHelper(GateConfig.DbConfig);
+            //NLogManager.LogMessage("Connect db: " + db.OpenConnection().State);
+            //db.Close();
         }
 
         private static string ByteToString(byte[] buff)
